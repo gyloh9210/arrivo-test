9a4d3e25fb60826f37769244c46a2a61ca52ae75
gyloh <ronaldo_loh@hotmail.com>
2023-11-12 22:39:56 +0800

    add login validation

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,28 +3,34 @@ const router = express.Router();
 const UserModel = require("../models/User");
 const { comparePassword } = require("../utils/crypto");
 const { generateAccessToken } = require("../utils/jwt");
+const { validate } = require("../middlewares/validation");
+const postLoginSchema = require("../validations/postLoginSchema");
 
-router.post("/login", async function (req, res, next) {
-  const { email, password } = req.body;
+router.post(
+  "/login",
+  validate(postLoginSchema),
+  async function (req, res, next) {
+    const { email, password } = req.body;
 
-  const user = await UserModel.findUser(email);
+    const user = await UserModel.findUser(email);
 
-  const isPasswordCorrect = await comparePassword(password, user.Password);
+    const isPasswordCorrect = await comparePassword(password, user.Password);
 
-  if (isPasswordCorrect) {
-    const token = generateAccessToken({
-      username: user.Username,
-      email: user.Email,
-      admin: user.Admin,
-      membership: user.Membership
+    if (isPasswordCorrect) {
+      const token = generateAccessToken({
+        username: user.Username,
+        email: user.Email,
+        admin: user.Admin,
+        membership: user.Membership,
+      });
+
+      return res.json({ token });
+    }
+
+    return res.status(401).json({
+      message: "Invalid credential",
     });
-
-    return res.json({ token });
   }
-
-  return res.status(401).json({
-    message: "Invalid credential"
-  });
-});
+);
 
 module.exports = router;
---
+++ middlewares/validation.js
@@ -0,0 +1,12 @@
+const validate = (schema) => async (req, res, next) => {
+  try {
+    await schema.validate(req.body);
+    return next();
+  } catch (err) {
+    return res.status(403).json({ type: err.name, message: err.message });
+  }
+};
+
+module.exports = {
+  validate,
+};
