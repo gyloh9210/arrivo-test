56bec6ba84a57dc145fc773c2242d54b6a29dc8d
gyloh <ronaldo_loh@hotmail.com>
2023-11-14 22:29:43 +0800

    add user login

---
+++ controllers/authController.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { validate } = require("../middlewares/validation");
+const postLoginSchema = require("../validations/postLoginSchema");
+const UserModel = require("../models/User");
+const { comparePassword } = require("../utils/crypto");
+const { generateAccessToken } = require("../utils/jwt");
+
+const router = express.Router();
+
+router.post("/login", validate(postLoginSchema), async function (req, res) {
+  const { email, password } = req.body;
+
+  const user = await UserModel.findUser(email);
+
+  if (!user) {
+    return res.status(401).json({
+      message: "User not found",
+    });
+  }
+
+  const isPasswordCorrect = await comparePassword(password, user.Password);
+
+  if (isPasswordCorrect) {
+    const token = generateAccessToken({
+      username: user.Username,
+      email: user.Email,
+      admin: user.Admin,
+      membership: user.Membership,
+    });
+
+    return res.json({ token });
+  }
+
+  return res.status(401).json({
+    message: "Invalid credential",
+  });
+});
+
+module.exports = router;
